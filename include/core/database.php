8289ccf3f13ce097c6d1dc70ce932411d1651a79
MaKa <amirhosein@maka-lap.(none)>
2013-03-12 11:36:12 +0330

    fix database

--- include/core/database.php
+++ include/core/database.php
@@ -50,33 +50,5 @@ class db_engine{
 		return ++$id;
 	}
 }
-?>
-=======
-<?php
-	require_once "../config.php";
-	class db_engine{
-		//variable
-		private $host;
-		private $db_name;
-		private $user;
-		private $pass;
-		//Function
-		public function __construct(){
-			$host = $_config['db']['host'];
-			$db_name = $_config['db']['name'];
-			$user = $_config['db']['user'];
-			$pass = $_config['db']['pass'];
-		}
-		private function connect(){
-			mysql_connect($host,$user,$pass) or die("Unable to connect database");
-		}
-		public function query($query){
-			this->connect();
-			mysql_escape_string($query);
-			if(mysql_query($query)){
-				return false;
-			}
-		}
-	}
-?>
->>>>>>> origin/master
+
+
